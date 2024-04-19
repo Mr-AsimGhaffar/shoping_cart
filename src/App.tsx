1c8304930fb93b32b60b081b7f8c615fe7f59e9e
Mr-AsimGhaffar <asim.ghaffar71@gmail.com>
2024-04-19 18:27:09 +0500

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-  return <div>Hello</div>;
-}
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  );
+};
 
 export default App;
